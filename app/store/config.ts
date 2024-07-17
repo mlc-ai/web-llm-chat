72105abd7af81d599e60f737d4d087c09179a436
Nestor Qin <imba.qxy@gmail.com>
2024-07-16 21:04:10 -0400

    feat: Add SmolLM models

--- app/store/config.ts
+++ app/store/config.ts
@@ -208,9 +208,9 @@ export const useAppConfig = createPersistStore(
   }),
   {
     name: StoreKey.Config,
-    version: 0.44,
+    version: 0.45,
     migrate: (persistedState, version) => {
-      if (version < 0.44) {
+      if (version < 0.45) {
         return {
           ...DEFAULT_CONFIG,
           ...(persistedState as any),
