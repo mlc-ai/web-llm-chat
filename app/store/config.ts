eca7b95b17583c61c43636bb3d13d6f415052371
Nestor Qin <imba.qxy@gmail.com>
2024-07-31 20:45:37 -0400

    model: add Gemma 2 models

--- app/store/config.ts
+++ app/store/config.ts
@@ -208,9 +208,9 @@ export const useAppConfig = createPersistStore(
   }),
   {
     name: StoreKey.Config,
-    version: 0.47,
+    version: 0.48,
     migrate: (persistedState, version) => {
-      if (version < 0.47) {
+      if (version < 0.48) {
         return {
           ...DEFAULT_CONFIG,
           ...(persistedState as any),
